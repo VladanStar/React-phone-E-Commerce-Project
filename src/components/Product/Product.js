14641e1d551556a76c31cb4efeb0bf6f01f53019
unknown <vladancupric@gmail.com>
2021-10-30 15:19:30 +0200

    ProductList start

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 export default class Product extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello from Product</h3>
-            </div>
+           <div>
+               Hello from Product
+           </div>
+           
         )
     }
 }
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
-import {Product} from "../Product/Product"
+import Product from '../Product/Product';
+import Title from '../Title/Title';
 
 export default class ProductList extends Component {
+
+    state ={
+        products:[]
+    };
+    
     render() {
         return (
-            <div>
-               <h3>Hello from ProductList</h3> 
-            </div>
+            <React.Fragment>
+
+            <div className="py-5">
+                <div className="container">
+
+                    <Title name="our" title="products"/>
+                    <div className="row">
+                        
+                    </div>
+                </div>
+             </div> 
+     
+         </React.Fragment>
         )
     }
 }
