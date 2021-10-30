b95e6b38c936cb6b6904969f0d1dbd2fe74df32f
unknown <vladancupric@gmail.com>
2021-10-30 09:40:16 +0200

    New commits

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,12 +5,27 @@ import logo from "../../logo.svg"
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-m navbar-dark px-sm-5">
+      <nav className="navbar navbar-expand-m bg-primary navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
             Creative Commons (Attribution 3.0 Unported);
                https://www.iconfinder.com/Makoto_msk */}
         <Link to="/">
             <img src={logo} alt="store" className="navbar=brand" />
+        </Link>
+        <ul className="navbar-nav align-items-center">
+            <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+           Products
+        </Link>
+        </li>
+
+        </ul>
+        <Link to="/cart" className="ml-auto">
+          <button>
+              <i className="fas fa-cart-plus"/>
+              my cart
+
+          </button>
         </Link>
       </nav>
     );
