4aa546dd6acb10d463645eab7bb7c465864e44f5
unknown <vladancupric@gmail.com>
2021-11-01 20:33:41 +0100

    New commits

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,7 @@ import CartColumns from "./CartColumns";
 import EmptyCart from "./EmptyCart";
 import { ProductConsumer } from "../../context";
 import CartList from "./CartList";
+import CartTotals from "./CartTotals";
 
 export default class Cart extends Component {
   render() {
@@ -18,7 +19,7 @@ export default class Cart extends Component {
                   <Title name="your" title="cart" />
                   <CartColumns />
                   <CartList value={value} />
-                  {/*<CartTotals value={value} history={this.props.history} /> */}
+                  <CartTotals value={value} history={this.props.history} /> 
                 </React.Fragment>
               );
             } else {
