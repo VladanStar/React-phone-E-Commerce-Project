b45c506593932a278b1fd3f88c51896395d36e6c
unknown <vladancupric@gmail.com>
2021-11-01 21:51:00 +0100

    Method Decrement Done

--- src/context.js
+++ src/context.js
@@ -71,10 +71,36 @@ export default class ProductProvider extends Component {
     });
   };
   increment = (id) => {
-    console.log("this is increment method");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => {
+      return item.id === id;
+    });
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count + 1;
+    product.total = product.count * product.price;
+    this.setState(() => {
+      return {
+        cart: [...tempCart]
+      };
+    }, this.addTotals);
   };
   decrement = (id) => {
-    console.log("this is decrement method");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => {
+      return item.id === id;
+    });
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count - 1;
+    if (product.count === 0) {
+      this.removeItem(id);
+    } else {
+      product.total = product.count * product.price;
+      this.setState(() => {
+        return { cart: [...tempCart] };
+      }, this.addTotals);
+    }
   };
   removeItem = (id) => {
     let tempProducts = [...this.state.products];
@@ -95,7 +121,11 @@ export default class ProductProvider extends Component {
         cart: [...tempCart],
         products: [...tempProducts],
       };
-    }, this.addTotals);
+    }, ()=>{
+      this.addTotals();
+    
+  }
+    )
   };
 
   clearCart = () => {
