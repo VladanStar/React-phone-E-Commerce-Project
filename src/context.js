e983f81d2336c6ab650a2eecfda8f33542dc7d33
unknown <vladancupric@gmail.com>
2021-11-01 21:28:42 +0100

    removedProduct Method Done

--- src/context.js
+++ src/context.js
@@ -11,11 +11,11 @@ export default class ProductProvider extends Component {
     products: [],
     detailProduct: detailProduct,
     cart: storeProducts,
-    modalOpen:false,
-    modalProduct:detailProduct,
-    cartSubTotal:0,
-    cartTax:0,
-    cartTotal:0
+    modalOpen: false,
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0,
   };
   componentDidMount() {
     this.setProducts();
@@ -49,57 +49,80 @@ export default class ProductProvider extends Component {
     product.count = 1;
     const price = product.price;
     product.total = price;
-    this.setState(() => {
-      return { products: tempProducts, cart:[...this.state.cart, product] };
-    },
-    ()=>{
-      this.addTotals();
-    });
+    this.setState(
+      () => {
+        return { products: tempProducts, cart: [...this.state.cart, product] };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
 
-
-  openModal =(id)=>{
-      const product = this.getItem(id);
-      this.setState(()=>{
-        return {modalProduct:product, modalOpen:true}
-      })
-  }
-  closeModal =()=>{
-    this.setState(()=>{
-      return{modalOpen:false}
-    })
-
-  }
-  increment = (id)=>{
+  openModal = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { modalProduct: product, modalOpen: true };
+    });
+  };
+  closeModal = () => {
+    this.setState(() => {
+      return { modalOpen: false };
+    });
+  };
+  increment = (id) => {
     console.log("this is increment method");
-
-  }
-  decrement = (id)=>{
+  };
+  decrement = (id) => {
     console.log("this is decrement method");
+  };
+  removeItem = (id) => {
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
 
-  }
-  removeItem = (id)=>{
-    console.log('item remove')
-  }
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
 
-  clearCart = ()=>{
-    console.log("cart wascleared")
-  }
-  addTotals= ()=>{
+    tempCart = tempCart.filter((item) => {
+      return item.id !== id;
+    });
+
+    this.setState(() => {
+      return {
+        cart: [...tempCart],
+        products: [...tempProducts],
+      };
+    }, this.addTotals);
+  };
+
+  clearCart = () => {
+    this.setState(
+      () => {
+        return { cart: [] };
+      },
+      () => {
+        this.setProducts();
+        this.addTotals();
+      }
+    );
+  };
+  addTotals = () => {
     let subTotal = 0;
-    this.state.cart.map(item=>(subTotal += item.total))
+    this.state.cart.map((item) => (subTotal += item.total));
     const tempTax = subTotal * 0.2;
     const tax = parseFloat(tempTax.toFixed(2));
     const total = subTotal + tax;
     this.setState(() => {
-      return{
+      return {
         cartSubTotal: subTotal,
-        cartTax:tax,
-        cartTotal:total
-      }
-    })
-    
-  }
+        cartTax: tax,
+        cartTotal: total,
+      };
+    });
+  };
   // tester = () => {
   //   console.log("State products:", this.state.products[0].inCart);
   //   console.log("Data products:", storeProducts[0].inCart);
@@ -123,13 +146,12 @@ export default class ProductProvider extends Component {
           ...this.state,
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
-          openModal:this.openModal,
-          closeModal:this.closeModal,
+          openModal: this.openModal,
+          closeModal: this.closeModal,
           increment: this.increment,
-          decrement:this.decrement,
-          removeItem:this.removeItem,
-          clearCart:this.clearCart,
-
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart,
         }}
       >
         {/* <button onClick={this.tester}>test me</button> */}
